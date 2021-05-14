f4dda3081cbccbe0109f33412f031c28ce6ad076
nathan hains <nathanhains14@yahoo.com>
2021-05-14 13:20:24 -0400

    commit tree f5b36caa0f6f26834f15c88ae3c7478d29000197

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, str){
+    const match = array.filter(s => s.toLowerCase() === str.toLowerCase() )
+    return match
+}
+
+function fuzzyMatch(array, str){
+    const match = array.filter(s => s.toLowerCase()[0] === str.toLowerCase()[0] )
+    return match
+}
+
+function matchName(array, str){
+    const match = array.filter(s => s.name.toLowerCase() === str.toLowerCase() )
+    return match
+}
+
